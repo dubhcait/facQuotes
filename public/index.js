c9731f069ae964d6f4d15ddcefc40d1944a40f2e
dubhcait <dubhcait@live.ie>
2019-05-16 21:01:24 +0100

    final visual

--- public/index.js
+++ public/index.js
@@ -1,77 +1,23 @@
-const quotes = document.querySelectorAll('.quote')
-let display = document.getElementById('display')
+const quotes = document.querySelectorAll('.quote');
+const display = document.getElementById('display');
+const main  = document.querySelector('main');
+const time = 3000;
+let n = 0;
 
-display.appendChild(quotes[3]);
+const showQuotes = () => {
 
-console.log(quotes[2].childNodes[0])
+display.removeChild(display.childNodes[0]);    
+display.appendChild(quotes[n]);
 
+if (n<quotes.length-1)
+{
+n++;
+}
+else
+{
+n=0;
+}
+setTimeout("showQuotes()",time);
 
-// document.addEventListener('keydown', function(event) {
-//     var ekey = event.keyCode
-//     if( 37 === ekey ) {
-//       pauseQuote();
-//     previousQuote();
-//     } 
-//   if ( 39 === ekey ) {
-//        // right arrow
-//     pauseQuote();
-//     nextQuote();
-//     }});
-
-// var quotes = document.querySelectorAll('#Quotes .Quote');
-// var currentQuote = 0;
-// var QuoteInterval = setInterval(nextQuote,2000);
-
-// function nextQuote() {
-//     goToQuote(currentQuote+1);
-// }
-
-// function previousQuote() {
-//     goToQuote(currentQuote-1);
-// }
-
-// function goToQuote(n) {
-//     quotes[currentQuote].className = 'Quote';
-//     currentQuote = (n+Quotes.length)%Quotes.length;
-//     quotes[currentQuote].className = 'Quote showing';
-// }
-
-// var playing = true;
-// var pauseButton = document.getElementById('pause');
-
-// function pauseQuote() {
-//     pauseButton.textContent = '&#9658';
-//     playing = false;
-//     clearInterval(QuoteInterval);
-// }
-
-// function playQuoteshow() {
-//     pauseButton.textContent = '||';
-//     playing = true;
-//     QuoteInterval = setInterval(nextQuote,2000);
-// }
-
-// pauseButton.onclick = function() {
-//     if(playing) {
-//     pauseQuote();
-//   } else {
-//     playQuoteshow();
-//   }
-// };
-
-// var next = document.getElementById('next');
-// var previous = document.getElementById('previous');
-
-// next.onclick = function() {
-//     pauseQuote();
-//     nextQuote();
-// };
-// previous.onclick = function() {
-//     pauseQuote();
-//     previousQuote();
-// };
-
-
-
-
-
+}
+showQuotes();
